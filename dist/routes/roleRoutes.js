97e3c10023621078f0e46000ffecc4f8ff0e8db2
sushant102004 <sushant.dhiman9812@gmail.com>
2023-03-19 20:17:18 +0530

    Vercel Build

--- dist/routes/roleRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.roleRouter = void 0;
-const express_1 = require("express");
-const rolesController_1 = require("./../controller/rolesController");
-const roleRouter = (0, express_1.Router)();
-exports.roleRouter = roleRouter;
-roleRouter.route('/').post(rolesController_1.createNewRole).get(rolesController_1.getAllRoles);
-//# sourceMappingURL=roleRoutes.js.map
